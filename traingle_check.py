d5e824843ee99594ad63aaba6b806fd7ad76dbb1
natkrs <147814283+natkrs@users.noreply.github.com>
2023-10-13 16:49:06 +0200

    Utworzono za pomocą Colaboratory

---
+++ traingle_check.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOSvCJxKWy463sNhARTvMga",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/natkrs/testclass/blob/master/traingle_check.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "m03aA0g6gDxr"
+      },
+      "outputs": [],
+      "source": [
+        "def trojkat(a,b,c):\n",
+        "  return a+b>c and b+c>a and c + a > b\n"
+      ]
+    }
+  ]
+}
